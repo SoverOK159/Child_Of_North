57f09738827e781c86c05bd5450e64d99eaeac00
Laptop-Sover\reshe <reshetnyak83@gmail.com>
2020-08-03 20:05:54 +0300

    Scriptable Enveronment

--- Assets/Scripts/Scriptable/Environment/Environment.cs
+++ Assets/Scripts/Scriptable/Environment/Environment.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(fileName ="Environmetn Item", menuName ="Environment")]
 public class Environment : ScriptableObject
 {
+    [SerializeField] string eItemName;
 
+    public string EItemName 
+    {
+        get { return eItemName; }
+    }
 }
